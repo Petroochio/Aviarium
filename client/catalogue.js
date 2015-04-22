a4cf17294c3c7aac3bc02709e774abf80b5f460f
Peter Gyory <ptg3662@rit.edu>
2015-04-21 23:06:18 -0400

    Fix mongoose error

--- client/catalogue.js
+++ client/catalogue.js
@@ -2,8 +2,8 @@
 
 $(document).ready(function() {
 
-  $('.birdBox').on("click", function(e) {
+  /*$('.birdBox').on("click", function(e) {
         
-    });
-
-)};
+    });*/
+  console.log('ready');
+});
